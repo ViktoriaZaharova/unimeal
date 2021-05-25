e353a1c87872f210ed1bfd8f67f4901283cf4d25
Viktoria <zaharova181114@gmail.com>
2021-05-26 00:39:47 +0300

    add page

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,12 @@
-$(document).ready(function() {
-    console.log( "ready!" );
+$(".countdown-time").countdowntimer({
+    dateAndTime: "2021/05/27 00:00:00",
+    labelsFormat: true,
+    displayFormat: "MS",
+    // currentTime: true,
+});
+
+$('.reviews-slider').slick({
+    slidesToShow: 3,
+    dots: true,
+    arrows: false
 });
